48e6e057a8187a44e2cf92ff26f31bf7fbd77793
Rudy Ravelin <rudy@noukod.com>
2019-01-14 16:58:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,9 +7,8 @@ class App < Sinatra::Base
   end
 
   get '/square/:number' do
-    @num = params[:number].to_i
-    erb :square
-  end
+     "#{(params[:number].to_i ** 2).to_s}"
+   end
 
   get '/say/:number/:phrase' do
       "#{params[:phrase] * params[:number].to_i}"
