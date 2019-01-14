daa2b66cb0af5866600f83f47f1b519b0f13c5f5
Rudy Ravelin <rudy@noukod.com>
2019-01-14 16:56:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,14 +17,9 @@ class App < Sinatra::Base
     erb :say
   end
 #
-  get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @w1 = params[:word1]
-    @w2 = params[:word2]
-    @w3 = params[:word3]
-    @w4 = params[:word4]
-    @w5 = params[:word5]
-    erb :saywords
-  end
+get '/say/:word1/:word2/:word3/:word4/:word5' do
+ "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+end
 
   get '/:operation/:number1/:number2' do
       @number1, @number2, @operation = params[:number1].to_i, params[:number2].to_i, params[:operation]
