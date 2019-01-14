99371d23fce051a8ca739151d8f337d9a9b4a4d4
Rudy Ravelin <rudy@noukod.com>
2019-01-14 16:57:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,11 +12,10 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    @number = params[:number].to_i
-    @phrase = params[:phrase]
-    erb :say
-  end
-#
+      "#{params[:phrase] * params[:number].to_i}"
+    end
+
+
 get '/say/:word1/:word2/:word3/:word4/:word5' do
  "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
 end
