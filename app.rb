9e02af19401b3de08d9660a25792a0d2052d0956
Rudy Ravelin <rudy@noukod.com>
2019-01-14 16:45:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # get '/reversename/:name' do
-  #   @name = params[:name]
-  #   erb :reversename
-  # end
+  get '/reversename/:name' do
+    @name = params[:name]
+    erb :reversename
+  end
 
   # get '/square/:number' do
   #   @num = params[:number].to_i
